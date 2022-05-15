2bbc386a0fa53e5abcf2c3bcd85aa2903cdfe286
Xadiera DeLLaRosa <dxadie@yahoo.com>
2022-05-15 09:03:18 -0600

    added variables

---
+++ variables.rb
@@ -0,0 +1,24 @@
+# variable dog_name is declared and assigned to the String of "Quisat"
+dog_name = "Quisait"
+# variable dog_age is declared and assigned to the Integer of 12
+dog_age = 12
+# variable hungry_dog is declared and assigned to the Boolean of true
+hungry_dog = true
+
+# Three variables - dog_name, dog_age, and hungry_dog are printed out
+# Expected output:
+# "Quisait"
+# 10
+# true
+puts dog_name, dog_age, hungry_dog
+
+# Variable dog_name is reassigned to the String of "Chief"
+dog_name = "Chief"
+# variable dog_age is reassigned to the Integer of 3
+dog_age = 3
+
+#Three variables - dog_name, dog_age, and hungry_dog are printed out
+# Expected output:
+# "Chief"
+# true
+puts dog_name, dog_age, hungry_dog
